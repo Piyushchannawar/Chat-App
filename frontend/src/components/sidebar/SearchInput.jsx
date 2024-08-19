f6949010f644ef591e50053b3d2f32d655e13ebe
Piyush Channawar <channawarpiyush@gmail.com>
2024-08-19 22:30:47 +0530

    completed Search functionality

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -1,13 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoSearchSharp } from "react-icons/io5"
+import useConversation from '../../zustand/useConversation';
+import useGetConversations from '../../hooks/useGetConversation';
+import toast from 'react-hot-toast';
 
 const SearchInput = () => {
+  const [search,setSearch] = useState("");
+  const {setSelectedConversation} = useConversation();
+  const {conversations}= useGetConversations();
+   
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    if(!search) return;
+    if(search.length < 3){
+      return toast.error('Search term must at least 3 character long')
+    }
+
+    const conversation = conversations.find((c) => c.fullname.toLowerCase().includes(search.toLowerCase()));
+
+    if(conversation) {
+      setSelectedConversation(conversation);
+      setSearch("");
+    } else toast.error(`No user fount with name ${search}`)
+
+  }
+
+
   return (
-    <form className='flex items-center gap-2 bg-gray-800 p-2 rounded-full'>
+    <form onSubmit={handleSubmit} className='flex items-center gap-2 bg-gray-800 p-2 rounded-full'>
       <input
         type='text'
         placeholder='Search…'
         className='bg-gray-700 text-gray-300 border border-gray-600 rounded-full px-4 py-2 outline-none focus:ring-2 focus:ring-blue-600'
+        value={search}
+        onChange={(e)=> setSearch(e.target.value)}
       />
       <button
         type='submit'
